08b900de3b6e84374e5f495104bb9af4f63ce0bf
Ariful-Srity <arifulontor84@gmail.com>
2022-05-28 02:44:00 +0600

    added new part

--- index.js
+++ index.js
@@ -16,6 +16,41 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8qzdx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function run() {
     try {
         await client.connect();
@@ -61,11 +96,22 @@ async function run() {
 
         })
 
+
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+
         app.post('/reviews', async (req, res) => {
             const newItem = req.body;
             const result = await reviewCollection.insertOne(newItem);
             res.send(result);
-        })
+        });
+
+
+
+
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -83,32 +129,7 @@ async function run() {
 
 
         //my profile----------
-        app.put('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const userProfileInfo = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            let updatedUser;
-            if (userProfileInfo.role) {
-                updatedUser = {
-                    $set: {
-                        role: userProfileInfo.role
-                    }
-                }
-            }
-            else {
-                updatedUser = {
-                    $set: {
-                        location: userProfileInfo.location,
-                        linkedIn: userProfileInfo.linkedIn,
-                        education: userProfileInfo.education,
-                        phone: userProfileInfo.phone
-                    }
-                }
-            }
-            const result = await userCollection.updateOne(filter, updatedUser, options);
-            res.send(result);
-        });
+
 
 
     }
