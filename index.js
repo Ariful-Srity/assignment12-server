d08a6d3c519cdb7af865becb948116d72ccf3520
Ariful-Srity <arifulontor84@gmail.com>
2022-05-28 20:50:45 +0600

    add delete part from services

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
 
         app.post('/orders', async (req, res) => {
             const order = req.body;
